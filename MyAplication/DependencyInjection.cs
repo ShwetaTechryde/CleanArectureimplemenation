b6e275cdd28da3a1e915baa3772cda819b1bcdb4
Dell <sweta18singh@gmail.com>
2025-01-16 10:18:54 -0800

    DepencyInjection

--- MyAplication/DependencyInjection.cs
+++ MyAplication/DependencyInjection.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyAplication
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddApplicationID(this IServiceCollection services)
+        {
+            // Register your dependencies here
+            return services;
+        }
     }
 }
