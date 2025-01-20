2b8fbabab88173897f6c4e44333a5d217fcd05be
Dell <sweta18singh@gmail.com>
2025-01-20 10:54:41 -0800

    Add folder

---
+++ MyAppInfrasture/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAppInfrasture.Data
+{
+    public class AppDbContext
+    {
+    }
+}
